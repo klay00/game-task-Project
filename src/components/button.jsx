2882d45ad277c286c23c918d83dbe356fed17263
klay00 <aali74432@gmail.com>
2023-04-17 00:38:27 +0300

    using formik

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import '../App.css';
 function BouttonAdd({ name, type }) {
-    console.log('fdfd');
     return (
         <>
             <button className="botton-add" type={type}>{name}</button>
--- src/pages/LogInPage.jsx
+++ src/pages/LogInPage.jsx
@@ -1,11 +1,39 @@
-import InputLogIn from "../components/inputLogin";
+import { useFormik } from "formik";
+import * as Yup from 'yup';
+export default function LogInPage() {
 
-export default function LogInPage(){
-
-    return(
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: ''
+        }, onSubmit: value => {
+            alert(JSON.stringify(value));
+        }
+    })
+    return (
         <>
-       <InputLogIn/>
-       
+            <div>
+
+                <form onSubmit={formik.handleSubmit}>
+                    <input
+                        onChange={formik.handleChange}
+                        value={formik.values.email}
+                        type="text"
+                        name="email"
+                        placeholder="emil" />
+
+                    <input
+                        onChange={formik.handleChange}
+                        value={formik.values.password}
+                        type="password"
+                        name="password"
+                        placeholder="password" />
+
+                    <button type="submit">Login</button>
+                </form>
+
+            </div>
+
         </>
     )
 }
